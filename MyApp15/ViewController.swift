6a9c0e14be613f1ee9c74ef69e598b634acaf04b
Shang Cheng <mark@Shangde-MacBook-Pro.local>
2017-09-26 09:40:03 +0800

    123

--- MyApp15/ViewController.swift
+++ MyApp15/ViewController.swift
@@ -9,7 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imgView: UIImageView!
+    
+    @IBAction func doTest1(_ sender: Any) {
+        let url = URL(string: "http://www.iii.org.tw")
+        do {
+            let content = try String(contentsOf: url!)
+            print(content)
+        }catch {
+            print("Error")
+        }
+        
+    }
+    
+    @IBAction func doTest2(_ sender: Any) {
+        let url = URL(string: "http://i2.cdn.cnn.com/cnnnext/dam/assets/170908160636-nk-icbm-5-large-tease.jpg")
+        do{
+            let data = try Data(contentsOf: url!)
+            let img = UIImage(data: data)
+            imgView.image = img
+            print("OK")
+        }catch {
+            print("Error2")
+        }
+        
+    }
+    
+    @IBAction func doTest3(_ sender: Any) {
+    }
+    
+    @IBAction func doTest4(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
